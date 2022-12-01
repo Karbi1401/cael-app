f7121f63ce621331f6d3463a1a8195afe6bfa2b9
Patrick Arganza <Arganzapatrick@gmail.com>
2022-12-02 03:19:39 +0800

    Update index.php

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -113,7 +113,120 @@
       </div>
     </div>
   </section>
+
+  <section id="testimonials" class="container team-section mt-2 mb-5">
+
+<!--Secion heading-->
+<div class="row mt-5 mb-4" >
+  <div class="col-md-12  wow jackInTheBox" data-wow-delay="0.2s">
+    <div class="divider-new">
+      <h3 class="text-center text-uppercase font-weight-bold mr-3 ml-3">Our
+        Guests opinions</h3>
+    </div>
+  </div>
+  <!--/Secion heading-->
+
+  <!--Section description-->
+  <p class="text-center grey-text w-responsive mx-auto mb-5 wow zoomIn" data-wow-delay="0.5s" >Lorem ipsum dolor
+    sit amet, consectetur adipisicing elit. Fugit, error amet numquam iure provident voluptate
+    esse quasi, veritatis totam voluptas nostrum quisquam eum porro a pariatur accusamus veniam.
+  </p>
+
+  <!--First row-->
+  <div class="row text-center">
+
+    <!--First column-->
+    <div class="col-md-4 mb-4 wow bounceInUp" data-wow-delay = "0.5s">
+
+      <div class="testimonial">
+        <!--Avatar-->
+        <div class="avatar mx-auto mb-4">
+          <img src="https://mdbootstrap.com/img/Photos/Avatars/img%20%285%29.jpg" class="rounded-circle img-fluid z-depth-1-half">
+        </div>
+
+        <!--Content-->
+        <h4 class="font-weight-bold">Anna Deynah</h4>
+        <p>
+          <i class="fas fa-quote-left "></i> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod eos
+          id officiis hic
+          tenetur quae quaerat ad velit ab.
+        </p>
+
+        <!--Review-->
+        <div class="orange-text">
+          <i class="fas fa-star"> </i>
+          <i class="fas fa-star"> </i>
+          <i class="fas fa-star"> </i>
+          <i class="fas fa-star"> </i>
+          <i class="fas fa-star-half-alt"> </i>
+        </div>
+      </div>
+    </div>
+    <!--/First column-->
+
+    <!--Second column-->
+    <div class="col-md-4 mb-4 wow bounceInUp" data-wow-delay="0.8s">
+      <div class="testimonial">
+        <!--Avatar-->
+        <div class="avatar mx-auto mb-4">
+          <img src="https://mdbootstrap.com/img/Photos/Avatars/img%20%289%29.jpg" class="rounded-circle img-fluid z-depth-1-half">
+        </div>
+
+        <!--Content-->
+        <h4 class="font-weight-bold">John Doe</h4>
+        <p>
+          <i class="fas fa-quote-left"></i> Ut enim ad minima veniam, quis nostrum exercitationem ullam
+          corporis suscipit laboriosam,
+          nisi ut aliquid ex ea commodi.</p>
+
+        <!--Review-->
+        <div class="orange-text">
+          <i class="fas fa-star"> </i>
+          <i class="fas fa-star"> </i>
+          <i class="fas fa-star"> </i>
+          <i class="fas fa-star"> </i>
+          <i class="fas fa-star"> </i>
+        </div>
+      </div>
+    </div>
+    <!--/Second column-->
+
+    <!--Third column-->
+    <div class="col-md-4 mb-4 wow bounceInUp" data-wow-delay="1.1s">
+      <div class="testimonial">
+        <!--Avatar-->
+        <div class="avatar mx-auto mb-4">
+          <img src="https://mdbootstrap.com/img/Photos/Avatars/img%20%288%29.jpg" class="rounded-circle img-fluid z-depth-1-half">
+        </div>
+        <!--Content-->
+        <h4 class="font-weight-bold">Tom Smith</h4>
+        <p>
+          <i class="fas fa-quote-left"></i> At vero eos et accusamus et iusto odio dignissimos ducimus qui
+          blanditiis praesentium
+          voluptatum deleniti atque corrupti.</p>
+
+        <!--Review-->
+        <div class="orange-text">
+          <i class="fas fa-star"> </i>
+          <i class="fas fa-star"> </i>
+          <i class="fas fa-star"> </i>
+          <i class="fas fa-star"> </i>
+          <i class="far fa-star"> </i>
+        </div>
+
+      </div>
+    </div>
+    <!--/Third column-->
+
+  </div>
+  <!--/First row-->
+</div>
+
+</section>
+<!--/Section: Testimonials v.3-->
+  
 </main>
+
 
 
 <?php require APPROOT . '/views/inc/footer.php'; ?>
