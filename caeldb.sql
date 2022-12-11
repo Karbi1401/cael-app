56b45cae4943f150ce4ebe44ed89b06a509ae8f3
Kurt Carvey Cadenas <kurtcarveycadenas1401@gmail.com>
2022-12-11 19:51:47 +0800

    Update caeldb.sql

--- caeldb.sql
+++ caeldb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 06, 2022 at 07:55 AM
+-- Generation Time: Dec 11, 2022 at 12:51 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.1.12
 
@@ -88,7 +88,11 @@ INSERT INTO `orderdetails` (`order_detail_id`, `order_id`, `product_id`, `produc
 (17, 96, 5, 'Breaded Fried Chicken', '150', '1', 5, 43, '2022-12-05 14:13:45'),
 (18, 96, 4, 'Beef & Mushroom', '300', '1', 5, 43, '2022-12-05 14:13:45'),
 (19, 97, 5, 'Breaded Fried Chicken', '150', '1', 5, 44, '2022-12-05 22:12:45'),
-(20, 97, 10, 'Stir Fry Vegetables', '150', '1', 5, 44, '2022-12-05 22:12:45');
+(20, 97, 10, 'Stir Fry Vegetables', '150', '1', 5, 44, '2022-12-05 22:12:45'),
+(21, 98, 4, 'Beef & Mushroom', '300', '1', 10, 45, '2022-12-09 21:55:42'),
+(22, 98, 5, 'Breaded Fried Chicken', '150', '1', 10, 45, '2022-12-09 21:55:42'),
+(23, 99, 4, 'Beef & Mushroom', '300', '1', 5, 46, '2022-12-09 22:28:36'),
+(24, 100, 4, 'Beef & Mushroom', '300', '1', 5, 47, '2022-12-09 22:55:35');
 
 -- --------------------------------------------------------
 
@@ -113,8 +117,11 @@ CREATE TABLE `orders` (
 --
 
 INSERT INTO `orders` (`order_id`, `order_schedule`, `user_id`, `rider_id`, `shipping_id`, `payment_id`, `order_total`, `order_status`, `created_at`) VALUES
-(96, '2022-12-05 14:13:00', 5, 2, 43, 43, '450', 2, '2022-12-05 14:13:45'),
-(97, '2022-12-05 23:12:00', 5, 2, 44, 44, '300', 2, '2022-12-05 22:12:45');
+(96, '2022-12-05 14:13:00', 5, 2, 43, 43, '450', 3, '2022-12-05 14:13:45'),
+(97, '2022-12-05 23:12:00', 5, 0, 44, 44, '300', 4, '2022-12-05 22:12:45'),
+(98, '2022-12-09 23:00:00', 10, 0, 45, 45, '450', 1, '2022-12-09 21:55:42'),
+(99, '2022-12-09 22:28:00', 5, 0, 46, 46, '300', 1, '2022-12-09 22:28:36'),
+(100, '2022-12-09 22:55:00', 5, 0, 47, 47, '300', 1, '2022-12-09 22:55:35');
 
 -- --------------------------------------------------------
 
@@ -136,7 +143,10 @@ CREATE TABLE `payments` (
 
 INSERT INTO `payments` (`payment_id`, `payment_method`, `payment_status`, `shipping_id`, `created_at`) VALUES
 (43, 'cash', 1, 43, '2022-12-05 14:13:45'),
-(44, 'cash', 1, 44, '2022-12-05 22:12:45');
+(44, 'cash', 2, 44, '2022-12-05 22:12:45'),
+(45, 'cash', 0, 45, '2022-12-09 21:55:42'),
+(46, 'cash', 0, 46, '2022-12-09 22:28:36'),
+(47, 'cash', 0, 47, '2022-12-09 22:55:35');
 
 -- --------------------------------------------------------
 
@@ -216,7 +226,10 @@ CREATE TABLE `shippings` (
 
 INSERT INTO `shippings` (`shipping_id`, `shipping_first_name`, `shipping_last_name`, `shipping_email`, `shipping_contact`, `shipping_address`, `shipping_city`, `created_at`) VALUES
 (43, 'Patrick', 'Arganza', 'banz@gmail.com', '09772880179', '#7 Belfast St. Vista Verde North Executive Village, Brgy 167, Caloocan City', 'Caloocan City', '2022-12-05 14:13:45'),
-(44, 'Patrick', 'Arganza', 'banz@gmail.com', '09772880179', '#7 Belfast St. Vista Verde North Executive Village, Brgy 167, Caloocan City', 'Caloocan City', '2022-12-05 22:12:45');
+(44, 'Patrick', 'Arganza', 'banz@gmail.com', '09772880179', '#7 Belfast St. Vista Verde North Executive Village, Brgy 167, Caloocan City', 'Caloocan City', '2022-12-05 22:12:45'),
+(45, 'Alixhan', 'Bustamante', 'alixhan24@gmail.com', '09772880193', '#4 Belfast St. Vista Verde North Executive Village, Brgy 167, Caloocan City', 'Caloocan City', '2022-12-09 21:55:42'),
+(46, 'Patrick', 'Arganza', 'arganza.patrickeugene@ue.edu.ph', '09772880179', '#7 Belfast St. Vista Verde North Executive Village, Brgy 167, Caloocan City', 'Caloocan City', '2022-12-09 22:28:36'),
+(47, 'Patrick', 'Arganza', 'arganza.patrickeugene@ue.edu.ph', '09772880179', '#7 Belfast St. Vista Verde North Executive Village, Brgy 167, Caloocan City', 'Caloocan City', '2022-12-09 22:55:35');
 
 -- --------------------------------------------------------
 
@@ -236,6 +249,8 @@ CREATE TABLE `users` (
   `user_password` varchar(255) NOT NULL,
   `user_role` varchar(255) NOT NULL DEFAULT 'user',
   `user_image` text NOT NULL DEFAULT 'default_avatar.png',
+  `token` text NOT NULL DEFAULT 'f1bbe1a05ef4ae3a2c16c422f0d934920aec4a6cecd5ca14d32aee806c3943ca4c6b4ccd1390262abaa61d733a1c5e1feffc',
+  `token_expire` datetime NOT NULL DEFAULT current_timestamp(),
   `created_at` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -243,10 +258,11 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`user_id`, `user_first_name`, `user_last_name`, `user_email`, `user_contact`, `user_address`, `user_city`, `user_username`, `user_password`, `user_role`, `user_image`, `created_at`) VALUES
-(5, 'Patrick', 'Arganza', 'banz@gmail.com', '09772880179', '#7 Belfast St. Vista Verde North Executive Village, Brgy 167, Caloocan City', 'Caloocan City', 'banz', '$2y$10$6DidZz8a5UBNExJYLsRx9.wCGJ8NwbyoH.vZU2FnJrLnL/09Yvol2', 'user', '308762815_792867072048927_5535246069055020808_n.jpg', '2022-12-02 02:09:03'),
-(6, 'Kurt', 'Cadenas', 'kurtcarveycadenas1401@gmail.com', '09772880178', '#8 Belfast St. Vista Verde North Executive Village, Brgy 167, Caloocan City', 'Caloocan City', 'krtcrvy', '$2y$10$ZUPscgoHNJRw7G7FfWJGi.3rdg1UN4mLqnBZELoSneIKSS5oWFgZu', 'admin', 'default_avatar.png', '2022-12-02 02:12:04'),
-(7, 'Leigh', 'Quinton', 'leigh@gmail.com', '09772880171', '#9 Belfast St. Vista Verde North Executive Village, Brgy 167, Caloocan City', 'Caloocan City', 'leigh', '$2y$10$L4sTWOGBRtQ2uUMaKzqA9ef35sNVSegkiiOt1No.oCxwVr1uVsSZq', 'employee', 'default_avatar.png', '2022-12-06 14:31:14');
+INSERT INTO `users` (`user_id`, `user_first_name`, `user_last_name`, `user_email`, `user_contact`, `user_address`, `user_city`, `user_username`, `user_password`, `user_role`, `user_image`, `token`, `token_expire`, `created_at`) VALUES
+(5, 'Patrick', 'Arganza', 'arganza.patrickeugene@ue.edu.ph', '09772880179', '#7 Belfast St. Vista Verde North Executive Village, Brgy 167, Caloocan City', 'Caloocan City', 'patrick', '$2y$10$TY0uS05fa9b8VRZTt6o7genmR8Td603H7M5IjTJcDwddxktO4ZLha', 'user', '308762815_792867072048927_5535246069055020808_n.jpg', 'a023749abaff93ab3f29f016f8940d1a8b98da35056e059b476e7a14519f42d3ec74a12b9dc283f4bfd3c9ebef6cb2b8b565', '2022-12-09 22:28:55', '2022-12-02 02:09:03'),
+(6, 'Kurt', 'Cadenas', 'kurtcarveycadenas1401@gmail.com', '09772880178', '#8 Belfast St. Vista Verde North Executive Village, Brgy 167, Caloocan City', 'Caloocan City', 'krtcrvy', '$2y$10$ZUPscgoHNJRw7G7FfWJGi.3rdg1UN4mLqnBZELoSneIKSS5oWFgZu', 'admin', 'default_avatar.png', 'f1bbe1a05ef4ae3a2c16c422f0d934920aec4a6cecd5ca14d32aee806c3943ca4c6b4ccd1390262abaa61d733a1c5e1feffc', '2022-12-09 20:11:47', '2022-12-02 02:12:04'),
+(7, 'Leigh', 'Quinton', 'leigh@gmail.com', '09772880171', '#9 Belfast St. Vista Verde North Executive Village, Brgy 167, Caloocan City', 'Caloocan City', 'leigh', '$2y$10$L4sTWOGBRtQ2uUMaKzqA9ef35sNVSegkiiOt1No.oCxwVr1uVsSZq', 'employee', 'default_avatar.png', 'f1bbe1a05ef4ae3a2c16c422f0d934920aec4a6cecd5ca14d32aee806c3943ca4c6b4ccd1390262abaa61d733a1c5e1feffc', '2022-12-09 20:11:47', '2022-12-06 14:31:14'),
+(10, 'Alixhan', 'Bustamante', 'alixhan24@gmail.com', '09772880193', '#4 Belfast St. Vista Verde North Executive Village, Brgy 167, Caloocan City', 'Caloocan City', 'ali', '$2y$10$sWzWoviX5Ubz.hIjt5ZDROw9lFsL9ooG5v2F7HXU3gvSNNRiVT2SW', 'user', 'default_avatar.png', 'a90c05ffbf37ff1329c02473fcc2fbe92cd2ec5b300853b8288a908bf20a23d6650cfee6f40a1b207c89b7856613cae223a1', '2022-12-09 22:56:49', '2022-12-09 21:55:08');
 
 --
 -- Indexes for dumped tables
@@ -314,7 +330,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `carts`
 --
 ALTER TABLE `carts`
-  MODIFY `cart_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+  MODIFY `cart_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 
 --
 -- AUTO_INCREMENT for table `categories`
@@ -326,19 +342,19 @@ ALTER TABLE `categories`
 -- AUTO_INCREMENT for table `orderdetails`
 --
 ALTER TABLE `orderdetails`
-  MODIFY `order_detail_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `order_detail_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=98;
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=101;
 
 --
 -- AUTO_INCREMENT for table `payments`
 --
 ALTER TABLE `payments`
-  MODIFY `payment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
+  MODIFY `payment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
 
 --
 -- AUTO_INCREMENT for table `products`
@@ -356,13 +372,13 @@ ALTER TABLE `riders`
 -- AUTO_INCREMENT for table `shippings`
 --
 ALTER TABLE `shippings`
-  MODIFY `shipping_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
+  MODIFY `shipping_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
